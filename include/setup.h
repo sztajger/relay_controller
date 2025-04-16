e9878eeba2fd35c098759266b190642d6089e33f
stajger <sztajgerland@gmail.com>
2025-04-16 14:19:00 +0200

    add button handler function

--- include/setup.h
+++ include/setup.h
@@ -1,14 +1,20 @@
 #ifndef SETUP_H
 #define SETUP_H
 
+#include "freertos/FreeRTOS.h"
 #include "driver/gpio.h"
 #include "driver/rtc_io.h"
+#include <freertos/projdefs.h>
 
 #define LED_PIN GPIO_NUM_2
 #define RELAY_PIN GPIO_NUM_18
 #define BUTTON_PIN GPIO_NUM_15
 #define BATTERY_PIN GPIO_NUM_34
 
-void setup(void);
+#define DEBOUNCE_TIME_MS 300
+
+void setup_gpio(void);
+void setup_timer(TimerHandle_t *debounce_timer);
+void setup_isr(TimerHandle_t debounce_timer);
 
 #endif
--- src/setup.c
+++ src/setup.c
@@ -1,9 +1,10 @@
 #include "setup.h"
+#include "button_handler.h"
+#include "esp_log.h"
 #include "driver/gpio.h"
-#include "driver/rtc_io.h"
 
 
-void setup(void)
+void setup_gpio(void)
 {
     // LED AND RELAY CONFIG AS OUTPUTS
     gpio_config_t io_conf =
@@ -23,13 +24,34 @@ void setup(void)
         .pin_bit_mask = (1ULL << BUTTON_PIN),
         .pull_up_en = GPIO_PULLUP_ENABLE,
         .pull_down_en = GPIO_PULLDOWN_DISABLE,
-        .intr_type = GPIO_INTR_DISABLE
+        .intr_type = GPIO_INTR_NEGEDGE, //For pullup resistor, if use pulldown GPIO_INTR_POSEDGE
     };
     gpio_config(&btn_conf);
 
     // START STATES
     gpio_set_level(LED_PIN, 0);
     gpio_set_level(RELAY_PIN, 1);
+}
 
-    // RTC FOR RELAY PIN (LATER)
+void setup_timer(TimerHandle_t *debounce_timer)
+{
+    *debounce_timer = xTimerCreate("debouncie_timer", pdMS_TO_TICKS(DEBOUNCE_TIME_MS), pdFALSE, NULL, debounce_timer_callback);
+    if (*debounce_timer == NULL)
+    {
+        ESP_LOGE("SETUP", "Failed to create debounce timer");
+        return;
+    }
+}
+void setup_isr(TimerHandle_t debounce_timer)
+{
+    if (gpio_install_isr_service(0) != ESP_OK)
+    {
+        ESP_LOGE("SETUP", "ISR service install failed");
+        return;
+    }
+
+    if (gpio_isr_handler_add(BUTTON_PIN, button_isr_handler, (void*) debounce_timer) != ESP_OK)
+    {
+        ESP_LOGE("SETUP", "ISR handler add failed");
+    }
 }
